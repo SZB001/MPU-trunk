c97cd0554ba92a72405d302f5690f6249a0b74c3
liu <531587156@qq.com>
2019-10-17 09:24:16 +0800

    【过程版本】:更新dbc--新增蓝牙一键启动请求、蓝牙一键启动状态信号

--- app/interface/hozon/PrvtProtocol/PrvtProt_SigParse.h
+++ app/interface/hozon/PrvtProtocol/PrvtProt_SigParse.h
@@ -48,7 +48,7 @@ description�� macro definitions
 #define PP_CANSIGN_OTAMODEFAILSTS    	0x17//
 #define PP_CANSIGN_PM25VALID    		0x18//空气净化器状态/pm2.5有效性
 #define PP_CANSIGN_CO2DENSITYSTS    	0x19//CO2浓度报警状态
-#define PP_CANSIGN_BLUETOOTHSTARTST    	0x1A//蓝牙一键启动
+#define PP_CANSIGN_BLUETOOTHSTARTST    	0x1A//蓝牙一键启动状态
 #define PP_MAX_RMTCTRL_CANSIGN_INFO   (PP_CANSIGN_BLUETOOTHSTARTST + 1)
 /***********�꺯��***********/
 
