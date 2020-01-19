906883c5e9edfec03b048163999a9e3f19950956
liu <531587156@qq.com>
2020-01-19 18:58:10 +0800

    【过程版本】：cfg中dbc默认版本改为r4_011WIP_v1.2

--- app/base/cfg/cfg_para.c
+++ app/base/cfg/cfg_para.c
@@ -29,7 +29,7 @@ author        liuzhongwen
 #include "dev_api.h"
 #include "udef_cfg_api.h"
 
-#define CFG_PARA_DBC_PATH   "/usrdata/dbc/GB-EP30_CAN_r4_011WIP_v1.1.dbc"
+#define CFG_PARA_DBC_PATH   "/usrdata/dbc/GB-EP30_CAN_r4_011WIP_v1.2.dbc"
 
 static unsigned char cfg_para_buf[CFG_PARA_BUF_LEN];
 static pthread_mutex_t cfg_para_mutex;
