28c91e0fdea82b0d3411b2f04b5b73a2e04b6e4b
liu <531587156@qq.com>
2020-05-28 08:56:18 +0800

    【过程版本】：pki库更新为1.1.23版本；去掉蓝牙库（跟tsp pki 库合并）

--- include/tboxsock.h
+++ include/tboxsock.h
@@ -16,7 +16,7 @@
 #define _INC_H_
 
 #define HOZON_TBOX_VERSION         0
-#define HOZON_TBOX_VERSION_TEXT    "TBOX SDK 1.1.22 - 2020-05-06"
+#define HOZON_TBOX_VERSION_TEXT    "TBOX SDK 1.1.23 - 2020-05-29"
 
 
 extern char *hurootcertstr;
@@ -26,6 +26,33 @@ extern char *huuserkeystr;
 extern unsigned int  pubPort;
 extern char         *pubIpAddr;
 
+extern char *approotcertstr;
+extern char *appmidcertstr;
+extern char *appusercertstr;
+extern char *appuserkeystr;
+
+/********************************* define return value *********************************************/
+#define CERTSETCFGFAIL             0
+#define CERTSETCFGOK               1
+/*-------------------------------------------------------------------------------------------------*/
+#define BACKUPFAIL                  0
+#define BACKUPOK                    1
+#define SYMENCRYPTFAIL              0
+#define SYMENCRYPTOK                1
+#define FILENOTOPEN                -1
+#define CERTVERIFYFAIL              0
+#define CERTVERIFYOK                1
+#define ROOTCERTLOADFAIL           -1
+#define CONVX509FAIL               -2     
+#define ROOTSTROETRUST             -3
+#define INITSCDCERT                -4
+#define CONVSECONCERTFAIL          -5
+#define SECONSTROETRUST            -6
+#define VERIFYCERTFAIL             -7
+#define KEYGENFAIL                 -8
+
+/********************************* BASE64 *********************************************/      
+#define HZBASE64 1
 
 
 /**************************************  Struct   *************************************************/
@@ -38,6 +65,17 @@ struct hz_vehicle_info_st{
 typedef struct hz_vehicle_info_st CAR_INFO;
 
 #define CIPHERS_LIST "ALL:!eNULL:!ADH:!PSK:!IDEA:!AES256:!AES128:!SRP:!SSLv3:!GMTLSv1.1:+DHE:+ECDSA:+SHA256:+CHACHA20:+CAMELLIA256"
+
+struct hzBT_vehicle_info_st{
+    char *unique_id;
+    char *tty_type;
+    char *carowner_acct;
+    char *impower_acct;
+};
+typedef struct hzBT_vehicle_info_st CARBT_INFO;
+
+#define CIPHERSBT_LIST "ALL:!eNULL:!ADH:!PSK:!IDEA:!AES256:!AES128:!SRP:!SSLv3:!GMTLSv1.1:+DHE:+ECDSA:+SHA256:+CHACHA20:+CAMELLIA256"
+
 
 /************************************** public Functions *************************************************/
 int HzPortAddrCft(unsigned int iPort, int mode, char *sIpaddr, char *sHostnameaddr);
@@ -151,6 +189,29 @@ int HzTboxSetVin(char *in_vin);
 
 //test
 //int HzTboxAesDecInfo( char *iFile, char *keynum);
+
+/**/
+/************************************** Functions *************************************************/
+int HzBtBackup(char *blueaddre, char *bluename, char *ccid, char *path);
+
+int HzBtVerifyBackup(char *blueaddre, char *bluename, char *ccid, char *path);
+
+int HzBtCertcfg(char *RtcertPath, char *ScdCertPath);
+
+int HzBtSymEncrypt(char *raw_buf, int len, char *result_buf, int *result_len, char *userkey, int type);
+
+int HzRequestInfo(char *cert, char *crlfile, char *Vin, char *UserID,  char *AuthorID, char *CodeInfo,  char *BackInfo, int *info_len, char *sekey,int *se_len);
+
+int HzBtGenSymKey(char *cert, char *chiperkey, char *plainkey,int *ch_len, int *p_len);
+
+
+
+int HZBase64Encode(unsigned char *dst, int *dlen, const unsigned char *src, int  slen);
+
+int HZBase64Decode(unsigned char *dst, int *dlen, const unsigned char *src, int  slen);
+
+int showversion(char *version);
+
 
 #endif  // _INC_H__
 
